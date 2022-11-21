17b2124390074564e1f2c385339185491860593f
SKY <dev@skyalty.com>
2022-11-21 23:39:51 +0100

    feat: rust html tag

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -21,5 +21,12 @@ require 'nvim-treesitter.configs'.setup {
   },
   autotag = {
     enable = true,
+    filetypes = {
+      'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
+      'xml',
+      'php',
+      'markdown',
+      'glimmer', 'handlebars', 'hbs', 'rust'
+    }
   },
 }
