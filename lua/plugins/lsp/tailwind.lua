60ac134c3d3a4a83715edba6c6aec5bf68464c18
SKY <dev@skyalty.com>
2023-01-17 17:09:22 +0100

    feat: working tailwindcss for rust yew templates

--- lua/plugins/lsp/tailwind.lua
+++ lua/plugins/lsp/tailwind.lua
@@ -20,6 +20,13 @@ require 'lspconfig'.tailwindcss.setup {
       '~H""".*class="([^"]*)".*"""',
     },
   },
+  init_options = {
+    userLanguages = {
+      eelixir = "html-eex",
+      eruby = "erb",
+      rust = "html",
+    },
+  },
   root_dir = require 'lspconfig'.util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js',
     'postcss.config.ts', 'package.json', 'node_modules'),
 }
