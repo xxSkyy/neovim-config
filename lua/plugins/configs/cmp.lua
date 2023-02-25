e6a824eb5e0a336d4dc392bb301dd59f1ef68210
SKY <dev@skyalty.com>
2023-02-25 03:26:17 +0100

    feat: cmp update

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -2,6 +2,12 @@
 local cmp = require 'cmp'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup {
   source_priority = {
     nvim_lsp = 1000,
@@ -21,15 +27,27 @@ cmp.setup {
   },
   mapping = {
     ['<Tab>'] = function(fallback)
+      local luasnip = require 'luasnip'
+
       if cmp.visible() then
         cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
     end,
     ['<S-Tab>'] = function(fallback)
+      local luasnip = require 'luasnip'
+
       if cmp.visible() then
         cmp.select_prev_item()
+      -- elseif luasnip.jumpable( -1) then
+      --   luasnip.jump( -1)
       else
         fallback()
       end
