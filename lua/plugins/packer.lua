a864054139b1bcab6e715f3e9ba3033a344bb086
sky <sky@iMac.local>
2023-03-07 02:10:39 +0100

    fix: add congfig for indent-blankline

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -285,9 +285,11 @@ require('packer').startup(function(use)
 
   -- Show indentation etc
   use { "lukas-reineke/indent-blankline.nvim",
+    config = function()
     neovim.require('indent_blankline', {
       show_end_of_line = true,
     })
+  end
   }
 
   -- Another commenting
