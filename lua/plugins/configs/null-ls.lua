d6b5613a486a96b64e669d632ec95cae64ec0da1
SKY <dev@skyalty.com>
2023-05-03 03:01:17 +0200

    feat: disable null_ls eslint

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -5,7 +5,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.luasnip,
     -- null_ls.builtins.completion.spell,
   },
