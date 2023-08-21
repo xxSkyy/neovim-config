120e321e5a876fb23425f0ea66eb542ab2c9f6a3
S K Y <dev@skyalty.com>
2023-08-21 13:13:21 +0200

    feat: add lsp eslint

---
+++ lua/plugins/lsp/eslint.lua
@@ -0,0 +1,9 @@
+require 'lspconfig'.eslint.setup({
+  capabilities = neovim.capabilities,
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
